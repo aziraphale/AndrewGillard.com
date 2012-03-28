95bcb7af073be0c54b61ce46d391442d256d686a
Andrew Gillard <lorddeath@gmail.com>
2012-03-28 16:48:56 +0100

    Trying to get custom routing to work.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -11,6 +11,12 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $layout = $this->getResource('layout');
         $view = $layout->getView();
         
+        $front = Zend_Controller_Front::getInstance();
+        $router = $front->getRouter();
+        $route = new Zend_Controller_Router_Route('blog/:id/:title', array('controller'=>'blog', 'action'=>'view'));
+        $router->addRoute('blogpost', $route);
+//        $router->addConfig(new Zend_Config_Ini(APPLICATION_PATH.'/configs/application.ini', 'production'), 'routes');
+        
         $config = new Zend_Config_Xml(APPLICATION_PATH.'/configs/navigation.xml', 'nav');
         $navigation = new Zend_Navigation($config);
         $view->navigation($navigation);
